7dfbc0c91892085fedc5678583c5e1b3adca271b
René Backhaus <rene.backhaus@real-digital.de>
2025-11-15 15:25:41 +0100

    Add working draft for shortest path

--- snippets/unweighted_paths/bfs_shortest_path.php
+++ snippets/unweighted_paths/bfs_shortest_path.php
@@ -27,12 +27,11 @@ function shortest_path_bfs(string $source, string $destination, array $graph): v
 		$parentNode = $dequeue[1] ?? null;
 
 		foreach ($graph[$currentNode] as $neighbor) {
-
+			// add the current path with cost to the map
 			if (!array_key_exists($currentNode, $pathMap)) {
-				// if a neighbor was already visited, use the cost from the path map
 				$cost = $pathMap[$parentNode][1] ?? 0;
 				$cost++;
-				$pathMap[$currentNode][0] = $neighbor;
+				$pathMap[$currentNode][0] = $parentNode;
 				$pathMap[$currentNode][1] = $cost;
 			}
 
@@ -42,10 +41,29 @@ function shortest_path_bfs(string $source, string $destination, array $graph): v
                 $queue[] = [$neighbor, $currentNode, 1];
             }
         }
+	    if ($currentNode === $destination) {
+		    break;
+	    }
     }
 	echo 'Path and Costs Map ' . PHP_EOL;
 	print_r($pathMap);
 
+	$path = [];
+	$node = $destination;
+
+	if (!isset($pathMap[$destination])) {
+		echo 'No path was found!';
+	}
+
+	while ($node !== null) {
+		$path[] = $node;
+		$node = $pathMap[$node][0];
+	}
+
+	echo 'The shortest path is: ' . PHP_EOL;
+	echo implode(', ', array_reverse($path)) . PHP_EOL;
+	echo 'The total cost is: ' . PHP_EOL;
+	echo $pathMap[$destination][1] . PHP_EOL;
 }
 
 
