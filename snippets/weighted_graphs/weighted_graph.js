4ebd1d4afb5df33f0781b2e97a5f2ce025d314c1
René Backhaus <rene.backhaus@real-digital.de>
2025-11-16 13:08:30 +0100

    Define test cases for shortest-path algorithm on weighted and unweighted graphs using adjacency lists

--- snippets/weighted_graphs/weighted_graph.js
+++ snippets/weighted_graphs/weighted_graph.js
@@ -18,6 +18,8 @@ let weighted_graph_adjacency_matrix = [
     [0, 0, -5, 0, 8, 0],
 ];
 
+// path: (A -> C - F)
+// cost: 2
 let unweighted_graph_adjacency_list = [
     [1, 2],       // A0
     [0, 3, 4],     // B1
@@ -27,12 +29,16 @@ let unweighted_graph_adjacency_list = [
     [2, 4],       // F5
 ];
 
+// todo change graph, that the path with the most vertices is the fastest one
+//  path: (A -> B -> E -> F)
+// cost: 3
+
 // first the vertic, then the weight
 let weighted_graph_adjacency_list = [
-    [[1, 5], [2, 4]],           // A0
-    [[0, 5], [3, 3], [4, 10]],     // B1
-    [[0, 4], [5, -5]],           // C2
-    [[1, 3]],                 // D3
-    [[1, 10], [5, 8]],           // E4
-    [[2, -5], [4, 8]],           // F5
+    [[1, 5], [2, 4]],               // A0
+    [[0, 5], [3, 3], [4, 10]],      // B1
+    [[0, 4], [5, -5]],              // C2
+    [[1, 3]],                       // D3
+    [[1, 10], [5, 8]],              // E4
+    [[2, -5], [4, 8]],              // F5
 ];
